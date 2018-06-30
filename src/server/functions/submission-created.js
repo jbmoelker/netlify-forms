c9654d4b1632fd2326d5202290278f2196736af6
Jasper Moelker <j.b.moelker@activeids.nl>
2018-06-30 21:03:16 +0200

    Log body parsed as JSON

--- src/server/functions/submission-created.js
+++ src/server/functions/submission-created.js
@@ -1,5 +1,7 @@
 export const handler = (event, context, callback) => {
   const send = body => callback(null, { statusCode: 200, body: JSON.stringify(body) })
-  console.log(JSON.stringify({ event, context }, null, 2))
-  send({ event, context })
+
+  const body = JSON.parse(event.body)
+  console.log(JSON.stringify({ body }, null, 2))
+  send({ body })
 }
