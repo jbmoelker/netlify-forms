6ef5f00b3bff5b9c9cdaed12a40ee32a0ea11d72
Jasper Moelker <j.b.moelker@activeids.nl>
2018-07-04 17:39:58 +0200

    Log (event.)body.payload

--- src/server/functions/submission-created.js
+++ src/server/functions/submission-created.js
@@ -2,6 +2,10 @@ export const handler = (event, context, callback) => {
   const send = body => callback(null, { statusCode: 200, body: JSON.stringify(body) })
 
   const body = JSON.parse(event.body)
-  console.log(JSON.stringify({ body }, null, 2))
+  // console.log(JSON.stringify({ body }, null, 2))
+  console.log('\nevent.body.payload\n')
+  console.log(event.body.payload)
+  console.log('\nbody.payload\n')
+	console.log(body.payload)
   send({ body })
 }
