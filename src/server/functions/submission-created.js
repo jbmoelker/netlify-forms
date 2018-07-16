6d0f61a237c4539b6208fedd0c0b14e04079ba03
Jasper Moelker <j.b.moelker@activeids.nl>
2018-07-16 21:50:29 +0200

    log httpMethod of submission-created

--- src/server/functions/submission-created.js
+++ src/server/functions/submission-created.js
@@ -2,9 +2,7 @@ export const handler = (event, context, callback) => {
   const send = body => callback(null, { statusCode: 200, body: JSON.stringify(body) })
 
   const body = JSON.parse(event.body)
-  // console.log(JSON.stringify({ body }, null, 2))
-  console.log('\nevent.body.payload\n')
-  console.log(event.body.payload)
+  console.log('method:', event.httpMethod)
   console.log('\nbody.payload\n')
 	console.log(body.payload)
   send({ body })
